de6e5f9403a1f9e845f5982f7897c88b2c29e96a
AdrianCreate <adrianalexandru1307@gmail.com>
2024-07-29 16:00:59 +0300

    I wrote a method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
-Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
+﻿myMethod();
+void myMethod()
+{
+    Console.WriteLine("Now I have a method");
+}
