825522b1eeaacf29166acf293e76d33d1b35ab37
AdrianCreate <adrianalexandru1307@gmail.com>
2024-07-29 16:05:30 +0300

    Revert "I wrote a method"
    This reverts commit de6e5f9403a1f9e845f5982f7897c88b2c29e96a.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-﻿myMethod();
-void myMethod()
-{
-    Console.WriteLine("Now I have a method");
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
+Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
