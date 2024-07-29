86e0ddd987095120b26c84bd170a37f357bc7f47
AdrianCreate <adrianalexandru1307@gmail.com>
2024-07-29 16:12:23 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
-Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification"); Console.WriteLine("Hello, C# academy! One more modification");
+﻿void myFirstMethod ()
+{
+
+}
